0bde02a5c61493c7d444be8f2f31647d1c21f09c
sprdgx <96926256+sprdgx@users.noreply.github.com>
2023-03-12 20:25:28 +0100

    Delete mongoose.tsx

--- lib/mongoose.tsx
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export async function initMongooose() {
-    if (mongoose.connection.readyState === 1 ) {
-        return mongoose.connection.asPromise();
-    }
-    mongoose.set('strictQuery', false);
-return await mongoose.connect(process.env.MONGODB_URL!);
-}
